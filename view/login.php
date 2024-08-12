8b82ecd23a57de14d61044b704b67e01d7cc84ce
Jose Agudelo <agudelotoxcino@gmail.com>
2024-08-11 20:33:56 -0500

    Login Style

--- view/login.php
+++ view/login.php
@@ -1,13 +1,42 @@
+<?php
+    include_once('../model/connect.php');
+    include_once('../controller/controller_register.php');
+
+    $register = new controller_register();
+    $register->returnManejos();
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LOGIN</title>
+    <link rel="stylesheet" href="styles/login.css" />
+    <title>WePlot</title>
 </head>
 <body>
-    <p>
-        Todo melo
-    </p>
+    <div class="container">
+        <div class="image-container">
+            <img class="img" src="images/login.png" alt="Imagen de registro" />
+        </div>
+        <div class="login-info-container">
+            <h1 class="title">Inicia sesión en <span class="title-1">WePlot</span></h1>
+            <form class="input-container" action="register.php?action=register" method="post">
+                <div class="input-group full-width">
+                    <label for="email">Email*</label>
+                    <input class="input" type="email" id="email" name="email" />
+                </div>
+                <div class="input-group full-width">
+                    <label for="password">Contraseña*</label>
+                    <input class="input" type="password" id="password" name="password" />
+                </div>
+                <div class="input-group full-width">
+                    <button class="btn full-width" type="submit" id="send">Iniciar sesión</button>
+                </div>
+                <div class="input-group full-width">
+                    <p>¿No tienes cuenta? <a href="register.php" class="span">Regístrate aquí</a></p>
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
