a48e2ad18d81113bb94c2eb5f3b737d6484957fb
Jose Agudelo <agudelotoxcino@gmail.com>
2024-08-18 16:20:46 -0500

    MVC_Controller

--- controller/controller_register.php
+++ controller/controller_register.php
@@ -1,58 +1,25 @@
 <?php
+require_once("../model/connect.php");
+require_once("../model/validateRegister.php");
 
-require_once '../model/connect.php';
-require_once '../model/addUser.php';
+$name = $_POST['name'];
+$last_name = $_POST['last_name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$country = $_POST['country'];
+$password = $_POST['password'];
+$confirm_password = $_POST['confirm-password'];
+$food = $_POST['food'];
+$artist = $_POST['artist'];
+$place = $_POST['place'];
+$color = $_POST['color'];
+$role = 1;
+$conexion = new Conexion();
+$conn = $conexion->conMysql();
 
-class controller_register{
-    public function returnManejos(){
-        $action = isset($_GET['action']) ? $_GET['action'] : '';
-        if ($action == 'register') {
-            $this->register();
-        }
-    }
+$user = new User($conn);
 
-    public function register(){
-        $name = trim($_POST['name']);
-        $last_name = trim($_POST['last_name']);
-        $email = trim($_POST['email']);
-        $phone = trim($_POST['phone']);
-        $country = trim($_POST['country']);
-        $food = trim($_POST['food']);
-        $artist = trim($_POST['artist']);
-        $place = trim($_POST['place']);
-        $color = trim($_POST['color']);
-        $password = trim($_POST['password']);
-        $confirm_password = trim($_POST['confirm-password']);
-
-        if ($password !== $confirm_password) {
-            echo 'Las contraseñas no coinciden';
-            return;
-        }
-        
-        if (empty($name) || empty($last_name) || empty($password) || empty($phone) || empty($email) || empty($country) || empty($password) || empty($confirm_password)) {
-            echo 'Por favor, complete todos los campos';
-            return;
-        }
-
-        $email_regex = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/";
-        if (!preg_match($email_regex, $email)) {
-            echo 'La dirección de correo electrónico no es válida';
-            return;
-        }
-
-        $connection = new Conexion();
-        $user = new User($connection->conMysql());
-
-        if ($user->emailExist($email)) {
-            echo 'El correo electrónico ya está registrado';
-            return;
-        }
-
-        $mdPassword = password_hash($password, PASSWORD_DEFAULT);
-        $user->addUser($name, $last_name, $email, $phone, $country, $mdPassword, $food, $artist, $place, $color);
-        header('Location: login.php');
-        exit;
-    }
-
-}
-?>
+$user->incorrect_password($password, $confirm_password);
+/*$user->validateEmail($email);*/
+$user->emailExist($email);
+$user->addUser($name, $last_name, $email, $phone, $country, $password, $role, $food, $artist, $place, $color);
