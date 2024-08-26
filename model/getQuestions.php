6a3e16dbccfec61433733a5f0334e8ccd6c77579
Jose Agudelo <agudelotoxcino@gmail.com>
2024-08-26 15:45:21 -0500

    Model for Questions changed (added alerts for Questions Changed)

--- model/getQuestions.php
+++ model/getQuestions.php
@@ -2,12 +2,40 @@
 class Questions
 {
     private $connection;
+    private $questions = [];
 
-    public function __construct($connection)
+    public function __construct($connection, $questions = [])
     {
         $this->connection = $connection;
+        $this->questions = $questions;
     }
 
+    public function updateQuestions()
+    {
+        foreach ($this->questions as $id => $question) {
+            $stmt = $this->connection->prepare("UPDATE questions SET question = ? WHERE question_id = ?");
+            $stmt->bind_param("si", $question, $id);
+            $stmt->execute();
+            $stmt->close();
+        }
+        echo "
+                <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>
+                <script language='JavaScript'>
+                document.addEventListener('DOMContentLoaded', function() {
+                    Swal.fire({
+                        icon: 'success',
+                        title: '¡Preguntas actualizadas con éxito!',
+                        text: 'Las preguntas fueron modificadas correctamente.',
+                        confirmButtonColor: '#D63030',
+                        confirmButtonText: 'OK',
+                        timer: 6000
+                    }).then(() => {
+                        location.assign('../view/admin_panel.php');
+                    });
+                });
+                </script>";
+                exit();
+    }
     public function getQuestions()
     {
         $result = $this->connection->query("
@@ -25,6 +53,7 @@ class Questions
         return $questionArray;
         
     }
+
 
 }
 
