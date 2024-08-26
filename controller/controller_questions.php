dca4f4f57f467f633dc2a7859d779dce9db01877
Jose Agudelo <agudelotoxcino@gmail.com>
2024-08-26 15:43:34 -0500

    Controller for Questions Changed

--- controller/controller_questions.php
+++ controller/controller_questions.php
@@ -3,10 +3,26 @@
 require_once("../model/getQuestions.php");
 require_once("../model/connect.php");
 
-
 $connection = new Connection();
 $conn = $connection->conMysql();
 
-$updater = new Questions($conn);
-$questions = $updater->getQuestions();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $questionsToUpdate = [];
 
+    for ($i = 1; $i <= 4; $i++) {
+        $questionId = $_POST["question-{$i}-id"] ?? null;
+        $questionText = $_POST["question-{$i}"] ?? null;
+        
+        if (!empty($questionId) && !empty($questionText)) {
+            $questionsToUpdate[$questionId] = $questionText;
+        }
+    }
+
+    if (!empty($questionsToUpdate)) {
+        $questionsModel = new Questions($conn, $questionsToUpdate);
+        $questionsModel->updateQuestions();
+    }
+}
+
+$getQuestions = new Questions($conn);
+$questions = $getQuestions->getQuestions();
