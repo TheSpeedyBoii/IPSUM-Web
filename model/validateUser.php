c711444bdc69139527d8dda17e87c1befb7d9142
Jose Agudelo <agudelotoxcino@gmail.com>
2024-08-26 15:46:01 -0500

    Model for User changed (added Password validation and Gmail regex for Email)

--- model/validateUser.php
+++ model/validateUser.php
@@ -4,69 +4,72 @@ session_start();
 class User
 {
     private $email;
-
     private $password;
-    private $conexion;
+    private $connection;
 
 
-    public function __construct($email, $password, $conexion)
+    public function __construct($email, $password, $connection)
     {
         $this->email = $email;
         $this->password = $password;
-        $this->conexion = $conexion;
+        $this->connection = $connection;
     }
 
     public function validateLogin()
     {
-        $validate_login = $this->conexion->query("SELECT * FROM users WHERE email ='$this->email'");
+
+        $stmt = $this->connection->prepare("SELECT * FROM users WHERE email = ?");
+        $stmt->bind_param("s", $this->email);
+        $stmt->execute();
+        $validate_login = $stmt->get_result();
+
         if ($validate_login->num_rows > 0) {
             $user_DB = $validate_login->fetch_assoc();
             $validate_email = $user_DB['email'];
             $role = $user_DB['id_role'];
             $passHash = $user_DB['pass'];
+
             $_SESSION["Email"] = $validate_email;
             $_SESSION["Role"] = $role;
+
             if (password_verify($this->password, $passHash)) {
-                if ($role == 2){
-                    echo '<script>window.location.href="../view/admin_panel.php";</script>';
-                }else{
-                    echo '<script>window.location.href="../view/welcome.php";</script>';
+                if ($role == 2) {
+                    header("Location: ../view/admin_panel.php");
+                } else {
+                    header("Location: ../view/welcome.php");
                 }
                 exit;
             } else {
                 echo "
-                <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>
-                <script language='JavaScript'>
-                document.addEventListener('DOMContentLoaded', function() {
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Falla al comparar contraseñas',
-                        confirmButtonColor: '#D63030',
-                        confirmButtonText: 'OK',
-                        timer: 6000
-                    }).then(() => {
-                        location.assign('../view/login.php');
-                    });
-                });
-                </script>";
+            <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>
+            <script>
+            Swal.fire({
+                icon: 'error',
+                title: 'Falla al comparar contraseñas',
+                confirmButtonColor: '#D63030',
+                confirmButtonText: 'OK',
+                timer: 6000
+            }).then(() => {
+                location.assign('../view/login.php');
+            });
+            </script>";
                 exit();
             }
         }
+
         echo "
-            <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>
-            <script language='JavaScript'>
-            document.addEventListener('DOMContentLoaded', function() {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Usuario No Existe o Contraseña Incorrecta. Verifique Su Informacion',
-                    showCancelButton: false,
-                    confirmButtonColor: '#3085d6',
-                    confirmButtonText: 'OK',
-                    timer: 5000
-                  }).then(() => {
-                    location.assign('../view/login.php');
-                  });
-        });
-            </script>";
+    <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>
+    <script>
+    Swal.fire({
+        icon: 'error',
+        title: 'Usuario No Existe o Contraseña Incorrecta. Verifique Su Informacion',
+        showCancelButton: false,
+        confirmButtonColor: '#3085d6',
+        confirmButtonText: 'OK',
+        timer: 5000
+    }).then(() => {
+        location.assign('../view/login.php');
+    });
+    </script>";
     }
 }
